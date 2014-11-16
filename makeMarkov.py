e897571abc872ebead8a1cf2e9e392498c310590
Ningrui Li <ningrui.li@duke.edu>
2014-11-16 05:40:25 -0500

    added spaces to markov matrix and made output matrix file have everything on a new line

--- makeMarkov.py
+++ makeMarkov.py
@@ -38,25 +38,37 @@ def parse_cli():
  
 def createMarkovMatrix(trainText):
     trainText = trainText.lower()
-    markovMatrix = [[0 for i in range(26)] for i in range(26)]
+    markovMatrix = [[0 for i in range(27)] for i in range(27)]
     wrapAroundText = trainText + trainText[0]
     
     for charCount in range (1, len(trainText)):
         currentChar = trainText[charCount-1]
+        asciiCurrentChar = ord(currentChar)
         nextChar = trainText[charCount] 
-        if currentChar.isalpha() and nextChar.isalpha():
-            markovMatrix[ord(currentChar)-ord('a')][ord(nextChar)-ord('a')] += 1;
+        asciiNextChar = ord(nextChar)
+
+        if (currentChar.isalpha() or currentChar == ' ') and (nextChar.isalpha() or nextChar == ' '):
+            if asciiCurrentChar == 32:
+                asciiCurrentChar = 123
+            if asciiNextChar == 32:
+                asciiNextChar = 123 
+            #print asciiCurrentChar
+            #print asciiNextChar
+            markovMatrix[asciiCurrentChar-ord('a')][asciiNextChar-ord('a')] += 1;
 
     return markovMatrix
     
 def printMarkovMatrix(markovMatrix, markovOutFile):
     markovMatrixFile = open(markovOutFile, 'w')
-    for ascii in range(97, 123):
-        markovMatrixFile.write(chr(ascii) +  ' ')
+    for ascii in range(97, 124):
+        if not ascii == 123:
+            markovMatrixFile.write(chr(ascii) +  '\n')
+        else:
+            markovMatrixFile.write('space' +  '\n')
         #print markovMatrix[ascii-97]
-        for i in range(26):
+        for i in range(27):
             markovMatrix[ascii-97][i] = str(markovMatrix[ascii-97][i])
-        markovMatrixFile.write(' '.join(markovMatrix[ascii-97]))
+        markovMatrixFile.write('\n'.join(markovMatrix[ascii-97]))
         markovMatrixFile.write('\n')
 if __name__ == "__main__":
     main()
